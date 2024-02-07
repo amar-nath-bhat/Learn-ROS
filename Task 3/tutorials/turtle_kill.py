946e366d105cbb9e3a88ddf2570764ef024b302e
amarnathbhat <bhatamar8@gmail.com>
2024-02-07 20:27:49 +0530

    Update

--- Task 3/tutorials/turtle_kill.py
+++ Task 3/tutorials/turtle_kill.py
@@ -20,7 +20,6 @@ class BotController:
         self.vel_x = 2
         self.z = 2
 
-
     def pose_callback(self, data):
         self.turtle_pose = data
     
@@ -38,38 +37,18 @@ class BotController:
         vel_msg.angular.z = 0
         self.vel_pub.publish(vel_msg)
 
-    # def go_to_goal(self, goal_x, goal_y):
-    #     distance = sqrt((goal_x - self.turtle_pose.x)**2 + (goal_y - self.turtle_pose.y)**2)
-    #     rospy.loginfo(distance)
-    #     while distance > 0.1:
-    #         rospy.loginfo("I'm in go 2 goal while loops")
+    def go_to_goal(self, goal_x, goal_y):
+        distance = sqrt((goal_x - self.turtle_pose.x)**2 + (goal_y - self.turtle_pose.y)**2)
+        rospy.loginfo(distance)
+        while distance > 0.1:
+            rospy.loginfo("I'm in go 2 goal while loops")
 
-    #         linear_speed = distance
-    #         angular_speed = (atan2(goal_y - self.turtle_pose.y, goal_x - self.turtle_pose.x) - self.turtle_pose.theta)
+            linear_speed = distance
+            angular_speed = (atan2(goal_y - self.turtle_pose.y, goal_x - self.turtle_pose.x) - self.turtle_pose.theta)
 
-    #         self.move(linear_speed, angular_speed, 0.1)
+            self.move(linear_speed, angular_speed, 0.1)
 
-    #         distance = sqrt((goal_x - self.turtle_pose.x)**2 + (goal_y - self.turtle_pose.y)**2)
-
-    def go_to_goal(self, x, y):
-        while abs(x - self.turtle_pose.x) > 0.05 or abs(y - self.turtle_pose.y) > 0.05:
-            angle = atan2(y - self.turtle_pose.y, x - self.turtle_pose.x)
-            distance = sqrt((x - self.turtle_pose.x)**2 + (y - self.turtle_pose.y)**2)
-            self.velocity.linear.x = 0
-            self.velocity.angular.z = self.z * (angle - self.turtle_pose.theta)
-
-            if abs(angle - self.turtle_pose.theta) <= 0.05:
-                self.velocity.angular.z = 0
-
-                if distance > 0.05:
-                    self.velocity.linear.x = (self.vel_x / 2) * distance
-                else:
-                    self.velocity.linear.x = 0
-                    break
-
-            self.vel_pub.publish(self.velocity)
-            self.rate.sleep()
-        self.vel_pub.publish(self.velocity)
+            distance = sqrt((goal_x - self.turtle_pose.x)**2 + (goal_y - self.turtle_pose.y)**2)
 
     def kill_bot(self):
         try:
@@ -80,7 +59,9 @@ class BotController:
 
     def handle_func(self, req):
         self.go_to_goal(req.x, req.y)
+        self.rate.sleep()
         self.kill_bot()
+        self.rate.sleep()
         return spawn_killResponse()
 
 if __name__ == '__main__':
